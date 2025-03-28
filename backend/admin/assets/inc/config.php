2242fd344010b6262f6fc3dd13d5c0d26bd28278
Atancha-Himself <samuelatancha@gmail.com>
2025-03-28 07:16:28 +0300

    Add files via upload

---
+++ backend/admin/assets/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="root";
+$dbpass="";
+$host="localhost";
+$db="hmis_db";
+$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
---
+++ backend/admin/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                 <?php echo date('Y'); ?> &copy; Kijabe Inpatient Care Information System [ICIS] </a>
+            </div>
+
+        </div>
+    </div>
+</footer>
